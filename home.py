fcbeb837ef4cecfd618b9b4c9f329581e13982f8
Chandan Thota <91654686+chandanthota75@users.noreply.github.com>
2023-05-15 01:09:58 +0530

    Update home.py

--- home.py
+++ home.py
@@ -16,7 +16,7 @@ def main():
     st.write(result)
 
     st.write("The source code for the website is present in the below github link")
-    github_link = '<a href="https://github.com/chandanthota75/Phishing_url_checker"' \
+    github_link = '<a href="https://github.com/chandanthota75/Phishing-URL-Checker"' \
                   ' target="_blank" class="github_link" >Github Link</a>'
     st.markdown(github_link, unsafe_allow_html=True)
     st.write("Thank You for visiting our Web app, feel free to contact us")
