233abe4ef8004948b370071f7b2a70808e0fa029
Chandan Thota <91654686+chandanthota75@users.noreply.github.com>
2023-05-15 00:58:35 +0530

    Update contact_us.py

--- contact_us.py
+++ contact_us.py
@@ -58,6 +58,29 @@ phone = ["9870309656", "7676253668", "7981060514", "7981860686", "7016210624", "
 
 
 def main():
+    style_page = """
+    #MainMenu {
+        visibility: hidden;
+    }
+
+    h1 {
+        text-align: center;
+    }
+    
+    h2 {
+        text-align: center;
+    }
+    
+    h3 {
+        text-align: center;
+    }
+
+    footer {
+        visibility: hidden;
+    }
+    """
+    st.markdown(style_page, unsafe_allow_html=True)
+    
     columns = st.columns([0.75, 0.25])
     with columns[0]:
         st.title("Contact US")
