3c94e01e38ffe37a5711ddcbc9d48ad4ea99c22e
Chandan Thota <91654686+chandanthota75@users.noreply.github.com>
2023-05-15 01:16:10 +0530

    Update home.py

--- home.py
+++ home.py
@@ -11,7 +11,7 @@ def main():
     
     st.title("Home")
 
-    url = st.text_input("Please Enter the Website URL in the below text box")
+    url = st.text_input("Please Enter complete URL of the website to check its legitimacy")
     if url != "":
         result = model_implementation.main(url)
         st.write(result)
