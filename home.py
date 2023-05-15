8d639b03a6bc149a8ac091c273be68c8d59782c4
Chandan Thota <91654686+chandanthota75@users.noreply.github.com>
2023-05-15 09:05:40 +0530

    Update home.py

--- home.py
+++ home.py
@@ -20,6 +20,8 @@ def main():
     
     st.title("Home")
     
+    st.warning("Below model will only accept complete URL only. Please check the URL format before passing it to the URL input field below")
+    
     url = st.text_input("Please enter complete URL of the website to check its legitimacy")
     if url == "":
         pass
@@ -29,6 +31,12 @@ def main():
             st.success("The Entered website is a Legetimate website")
         else:
             st.warning("The Entered website is used for Phishing your data")
+            st.warning("A URL is a string of characters that provides a reference to a resource on the Internet. A complete URL consists of multiple components, including the scheme, the network location, and any additional path and query parameters.")
+            st.warning("The scheme specifies the protocol used to access the resource on the server, and is usually followed by a colon and two forward slashes ("://").")
+            st.warning("The network location specifies the domain name or IP address of the server where the resource is located, and is separated from the scheme by a forward slash ("/").")
+            st.warning("To check whether a URL is complete or not, you need to ensure that it includes both the scheme and the network location. If either of these components is missing, the URL is not complete.")
+            st.warning("A URL without a scheme (e.g., "www.example.com") is not complete because it is missing the scheme component.")
+            st.warning("A URL without a network location (e.g., "https://") is not complete because it is missing the network location component.")
     else:
         st.warning("The Entered URL is incomplete/wrong, please check the format of the entered URL again")
     
@@ -37,8 +45,7 @@ def main():
     st.write("To create a spoofed website, attackers may use techniques such as domain spoofing, where they register a domain name that is similar to the legitimate website's domain name, or they may use website cloning software to replicate the appearance and functionality of the legitimate website.")
     st.write("It's important to be cautious when entering personal information online and to always check the legitimacy of a website before entering any sensitive data. One way to do this is to look for the padlock icon in the browser's address bar, which indicates that the website is using a secure connection (HTTPS) and has a valid security certificate. Additionally, be wary of emails or messages that ask you to enter personal information or click on links, especially if they seem suspicious or out of the ordinary.")
     
-    st.write('This Webapp is curriculum-based project, which is constructed as a part of "Engineering Project in '
-         'Community Services" by the students of VIT Bhopal The Key contributors for the project are')
+    st.subheader('This Webapp is curriculum-based project, which is constructed as a part of "Engineering Project in Community Services" by the students of VIT Bhopal The Key contributors for the project are')
     st.write("The source code for the website is present in the below github link")
     github_link = '<a href="https://github.com/chandanthota75/Phishing-URL-Checker"' \
                   ' target="_blank" class="github_link" >Github Link</a>'
