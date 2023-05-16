596647c9d794d21218ad023a0dc4fee239c0f757
Chandan Thota <91654686+chandanthota75@users.noreply.github.com>
2023-05-16 09:39:42 +0530

    Update url_model_analysis.py

--- url_model_analysis.py
+++ url_model_analysis.py
@@ -7,50 +7,43 @@ import plotly.express as px
 def main():
     page_markdown = open("general_style.css", "r").read()
     st.markdown(f"<style>{page_markdown}</style>", unsafe_allow_html=True)
-    
+
     st.title("URL Model analysis")
 
-    st.write(f"We have used the dataset from the website which contains about 84 features which can be used to "
-             f"determine whether the website is used for phishing or not The dataset consists of 11430 urls which are "
-             f"categorized as legitimate or phishing links.")
-    kaggle_link = '<a href="https://www.kaggle.com/datasets/shashwatwork/web-page-phishing-detection-dataset"' \
-                  ' target="_blank" class="kaggle_link" >Kaggle Link</a>'
-    st.markdown(kaggle_link, unsafe_allow_html=True)
+    columns = st.columns([0.4, 0.6])
+    with columns[0]:
+        st.write("")
+        st.write(f"We have used the dataset from the website which contains about 84 features which can"
+                 f" be used to determine whether the website is used for phishing or not The dataset consists"
+                 f" of 11430 urls which are categorized as legitimate or phishing links.")
+        kaggle_link = '<a href="https://www.kaggle.com/datasets/shashwatwork/web-page-phishing-detection-dataset"' \
+                      ' target="_blank" class="kaggle_link" >Kaggle Link</a>'
+        st.markdown(kaggle_link, unsafe_allow_html=True)
 
-    st.write("")
-   
+        st.write("Preprocessing the dataset would insure better results and less training time for building the"
+                 " models. We have performed many preprocessing steps such as balancing the dataset using SMOTE"
+                 " Analysis, Dimensionality reduction using PCA, Data Transformation and Normalization, Feature"
+                 " Selection, etc.. Now the preprocessed dataset consists of 28 High correlated features which"
+                 " can be used for model building")
+    with columns[1]:
+        st.image(f"data:image/png;base64,{main_app.image_to_bytes('url_model_analysis/pie.png')}")
+
     columns = st.columns(2)
     with columns[0]:
         st.subheader("Before Preprocessing Dataset")
-        before_preprocess = pa.read_csv("url_model_analysis/WebsiteDatasets/phishing_before_preprocess.csv")
-        st.dataframe(data=before_preprocess)
+        before_preprocess = pa.read_csv("url_model_analysis/phishing_before_preprocess.csv")
+        st.dataframe(data=before_preprocess.set_index("url"))
     with columns[1]:
         st.subheader("After Preprocessing Dataset")
-        after_preprocess = pa.read_csv("url_model_analysis/WebsiteDatasets/phishing_after_preprocess.csv")
+        after_preprocess = pa.read_csv("url_model_analysis/phishing_after_preprocess.csv")
         st.dataframe(data=after_preprocess.set_index("id"))
 
-    st.write("")
-    st.write("Preprocessing the dataset would insure better results and less training time for building the models. "
-             "We have performed many preprocessing steps such as balancing the dataset using SMOTE Analysis, "
-             "Dimensionality reduction using PCA, Data Transformation and Normalization, Feature Selection, "
-             "etc.. Now the preprocessed dataset consists of 28 High correlated features which can be used for model "
-             "building")
-    st.write("")
-    
-    columns = st.columns(3)
-    with columns[1]:
-        st.image(f"data:image/png;base64,{main_app.image_to_bytes('url_model_analysis/WebsiteImages/after_preprocess.png')}", caption=None, channels="RGB")
-
-    st.write("We have used a Machine Learning, Ensemble Learning and Deep Learning and build and trained a total of "
-             "20 models. this models are evaluated and results are presented below")
-
-    columns = st.columns(2)
-
+    st.write("We have used a Machine Learning, Ensemble Learning and Deep Learning and build and trained a total"
+             " of 20 models. this models are evaluated and results are presented below")
     with columns[0]:
         st.subheader("Machine Learning Models")
 
-        ml = pa.read_csv("url_model_analysis/WebsiteDatasets/Machine Learning model results.csv")
-        st.table(data=ml.set_index('Model'))
+        ml = pa.read_csv("url_model_analysis/Machine Learning model results.csv")
 
         ml_colors = ['#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
@@ -62,11 +55,11 @@ def main():
         fig.update_layout(showlegend=False)
         st.plotly_chart(fig, use_container_width=True)
 
+        st.table(data=ml.set_index('Model'))
     with columns[1]:
         st.subheader("Deep Learning Models")
 
-        dl = pa.read_csv("url_model_analysis/WebsiteDatasets/Deep Learning model results.csv")
-        st.table(data=dl.set_index('Model'))
+        dl = pa.read_csv("url_model_analysis/Deep Learning model results.csv")
 
         dl_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd']
 
@@ -78,11 +71,13 @@ def main():
         fig.update_layout(showlegend=False)
         st.plotly_chart(fig, use_container_width=True)
 
+        st.table(data=dl.set_index('Model'))
+
     st.subheader("Ensemble Learning Models")
     columns = st.columns(2)
 
     with columns[0]:
-        el = pa.read_csv("url_model_analysis/WebsiteDatasets/Ensemble Learning model results.csv")
+        el = pa.read_csv("url_model_analysis/Ensemble Learning model results.csv")
         st.table(data=el.set_index('Model'))
 
     with columns[1]:
@@ -97,5 +92,6 @@ def main():
         fig.update_layout(showlegend=False)
         st.plotly_chart(fig, use_container_width=True)
 
-    st.write("We are going to take models which have highest Accuracy out of machine, ensemble and deep learning and "
-             "make accurate predictions")
+    st.write("We are going to take 2 models which have highest Accuracy out of machine, ensemble and deep "
+             "learning and make accurate predictions. So the total models which we going to use in the prediction"
+             " of URL Legetimcy is 6")
